7700f2103b2b95033e5a57b1370c6bc952908925
Zifan Xu <54227863+Daffan@users.noreply.github.com>
2020-08-19 15:46:24 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import gym
 import jackal_envs
 # from jackal_sim_wrapper import RandomStartGoalPosition, ReducedObservation, RewardShaping
 
-env = gym.make('jackal_navigation-v0', world_name = '75cm_split.world', gui = 'false', VLP16 = 'false', camera = 'false')
+env = gym.make('jackal_navigation-v0', world_name = '75cm_split.world', gui = 'false', VLP16 = 'false', init_position = [-2, -2, 0], goal_position = [2, 2, 0])
 # env = ReducedObservation(gym.make('jackal_navigation-v0', world_name = '85cm_split.world', gui = 'true', VLP16 = 'false'))
 # env = RandomStartGoalPosition(gym.make('jackal_navigation-v0', world_name = '85cm_split.world', gui = 'true', VLP16 = 'false'))
 # env = RewardShaping(gym.make('jackal_navigation-v0', world_name = 'sequential_applr_testbed.world', gui = 'true', VLP16 = 'false', init_position = [-8, 0, 0], goal_position = [54, 0, 0]))
@@ -13,7 +13,7 @@ done = False
 count = 0
 while not done:
     count += 1
-    obs, rew, done, _ = env.step(0)
+    obs, rew, done, _ = env.step([0, 0, 0, 0, 0, 0])
     print('current step %d' %(count))
     if count > 40:
         break
